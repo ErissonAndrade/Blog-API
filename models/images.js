17e080d258cf01153c2f551f554856fa548077ac
Erisson Andrade <erisson5000@hotmail.com>
2023-08-14 21:01:02 -0300

    Changed images model to URL instead of image

--- models/images.js
+++ models/images.js
@@ -3,14 +3,8 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const ImagesSchema = new Schema({
-    image: {type: Buffer, required: true},
+    imageURL: {type: String, required: true},
     name: {type: String, required: true}
 });
-
-ImagesSchema.virtual('imageURL').get(function() {
-    return `data:image/jpeg;base64,${this.image.toString('base64')}`;
-});
-
-ImagesSchema.set('toJSON', { virtuals: true });
 
 module.exports = mongoose.model('Images', ImagesSchema);
